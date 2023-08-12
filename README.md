3e0e33a6687964c339ec32de1058ea30e7ceb851
dylan <dylan001@Dylans-MacBook-Pro.local>
2023-08-12 18:20:29 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # format-money
+
 format money
+
+## Install
+
+```
+npm i @bearcookie/format-money
+or
+yarn add @bearcookie/format-money
+```
+
+## Usage
+
+```javascript
+import useMoney from "@bearcookie/format-money";
+const { divide, multipole } = useMoney()
+
+// divide
+const money = divide(123456)
+console.log(money.value) // 123,4.56
+console.log(money.real) // 1234.56
+console.log(money.origin) // 123456
+
+// multiple
+multipole(123456) // 12345600
+```
+
+### API
+
+```javascript
+
+```
---
+++ src/index.ts
@@ -0,0 +1,60 @@
+/**
+ * @description 格式化金额
+ * @param value 数值
+ * @param multiple 倍数，默认100
+ * @param decimal 小数点，默认2位
+ * @return 乘：整型数值，除：{ value: 格式化后的值, real: 整型数值, origin: 源始值 }
+ */
+
+class Money {
+  private multy: number = 100
+  private dec: number = 2
+
+  multiply = (value: number | string, multiple?: number, decimal?: number): number => {
+    const moneyDecimal: number = decimal ?? this.dec
+    const meneyMulty: number = multiple ?? this.multy
+    let money: number = 0
+
+    if (value !== '' && value !== null && value !== undefined) money = parseFloat((+value * meneyMulty).toFixed(moneyDecimal))
+    if (!isNaN(money)) return money
+    else return 0
+  }
+
+  divide = (value: number | string, multiple?: number, decimal?: number): { value: string; real: number; origin: number } => {
+    const moneyDecimal: number = decimal ?? this.dec
+    const meneyMulty: number = multiple ?? this.multy
+    const zero: string = Array(moneyDecimal).fill(0).join('')
+    let money: string = String(value)
+    let plain: number = 0
+
+    if (value !== '' && value !== null && value !== undefined) {
+      plain = parseFloat((+value / meneyMulty).toFixed(moneyDecimal))
+      const mount: string[] = String(plain).split('.')
+
+      if (mount[1] !== undefined) {
+        mount[1] = mount[1].padEnd(mount[1].length + moneyDecimal, zero)
+        mount[1] = String(mount[1]).substring(0, moneyDecimal)
+      } else {
+        mount[1] = zero
+      }
+      mount[0] = mount[0].replace(/\d+/, function (n: string) {
+        return n.replace(/(\d)(?=(\d{3})+$)/g, function (value: string): string {
+          return value + ','
+        })
+      })
+      money = mount.join('.')
+    } else {
+      money = '0.' + zero
+      plain = 0
+      value = 0
+    }
+
+    return {
+      value: money,
+      real: plain,
+      origin: Number(value)
+    }
+  }
+}
+
+export default () => new Money()
