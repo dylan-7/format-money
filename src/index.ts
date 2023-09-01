3178b0cc78fa22a6bacffb32928e7e70cb3c0b29
dylan <dylan001@Dylans-MacBook-Pro.local>
2023-09-01 16:17:06 +0800

    v1.0.1

--- src/index.ts
+++ src/index.ts
@@ -7,54 +7,62 @@
  */
 
 class Money {
-  private multy: number = 100
-  private dec: number = 2
+  private multy: number = 100;
+  private dec: number = 2;
 
   multiply = (value: number | string, multiple?: number, decimal?: number): number => {
-    const moneyDecimal: number = decimal ?? this.dec
-    const meneyMulty: number = multiple ?? this.multy
-    let money: number = 0
+    const moneyDecimal: number = decimal ?? this.dec;
+    const meneyMulty: number = multiple ?? this.multy;
+    const split = String(+value * meneyMulty).split('.');
+    const num = [split[0], '.', split[1] ? split[1].slice(0, moneyDecimal) : ''].join();
+    let money: number = 0;
 
-    if (value !== '' && value !== null && value !== undefined) money = parseFloat((+value * meneyMulty).toFixed(moneyDecimal))
-    if (!isNaN(money)) return money
-    else return 0
-  }
+    if (value !== '' && value !== null && value !== undefined) money = parseFloat(num);
+    if (!isNaN(money)) return money;
+    else return 0;
+  };
 
-  divide = (value: number | string, multiple?: number, decimal?: number): { value: string; real: number; origin: number } => {
-    const moneyDecimal: number = decimal ?? this.dec
-    const meneyMulty: number = multiple ?? this.multy
-    const zero: string = Array(moneyDecimal).fill(0).join('')
-    let money: string = String(value)
-    let plain: number = 0
+  divide = (
+    value: number | string,
+    multiple?: number,
+    decimal?: number,
+  ): { value: string; real: number; origin: number } => {
+    const moneyDecimal: number = decimal ?? this.dec;
+    const meneyMulty: number = multiple ?? this.multy;
+    const zero: string = Array(moneyDecimal).fill(0).join('');
+    let money: string = String(value);
+    let plain: number = 0;
 
     if (value !== '' && value !== null && value !== undefined) {
-      plain = parseFloat((+value / meneyMulty).toFixed(moneyDecimal))
-      const mount: string[] = String(plain).split('.')
+      const split: string[] = String(+value / meneyMulty).split('.');
+      const num: number = +[split[0], '.', split[1] ? split[1].slice(0, moneyDecimal) : ''].join();
+      plain = parseFloat(num.toFixed(moneyDecimal));
+      const mount: string[] = String(plain).split('.');
 
       if (mount[1] !== undefined) {
-        mount[1] = mount[1].padEnd(mount[1].length + moneyDecimal, zero)
-        mount[1] = String(mount[1]).substring(0, moneyDecimal)
+        mount[1] = mount[1].padEnd(mount[1].length + moneyDecimal, zero);
+        mount[1] = String(mount[1]).substring(0, moneyDecimal);
       } else {
-        mount[1] = zero
+        mount[1] = zero;
       }
       mount[0] = mount[0].replace(/\d+/, function (n: string) {
         return n.replace(/(\d)(?=(\d{3})+$)/g, function (value: string): string {
-          return value + ','
-        })
-      })
-      money = mount.join('.')
+          return value + ',';
+        });
+      });
+      money = mount.join('.');
     } else {
-      money = '0.' + zero
-      plain = 0
-      value = 0
+      money = '0.' + zero;
+      plain = 0;
+      value = 0;
     }
 
     return {
       value: money,
       real: plain,
-      origin: Number(value)
-    }
-  }
+      origin: Number(value),
+    };
+  };
 }
 
-export default () => new Money()
+export default () => new Money();
