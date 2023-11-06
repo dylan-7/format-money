d4eecf4a9bfc01ce5ca86a0e73a5e6b97ecb8db8
dylan <dylan001@Dylans-MacBook-Pro.local>
2023-11-06 22:50:01 +0800

    v1.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,38 @@
 /**
- * @description 格式化金额
+ * @description 格式化金钱
  * @param value 数值
  * @param multiple 倍数，默认100
  * @param decimal 小数点，默认2位
- * @return 乘：整型数值，除：{ value: 格式化后的值, real: 整型数值, origin: 源始值 }
+ * @return 乘：整型数值，除：{ value: 格式化的值, real: 整型数值, origin: 源始值 }
  */
 
 class Money {
-  private multy: number = 100;
-  private dec: number = 2;
+  multy = 100;
+  dec = 2;
 
-  multiply = (value: number | string, multiple?: number, decimal?: number): number => {
-    const moneyDecimal: number = decimal ?? this.dec;
-    const meneyMulty: number = multiple ?? this.multy;
-    const split = String(+value * meneyMulty).split('.');
-    const num = [split[0], '.', split[1] ? split[1].slice(0, moneyDecimal) : ''].join();
-    let money: number = 0;
+  multiply = (value: number | string, multiple?: number, decimal?: number) => {
+    const moneyDecimal = decimal ?? this.dec;
+    const meneyMulty = multiple ?? this.multy;
+    let money = 0;
 
-    if (value !== '' && value !== null && value !== undefined) money = parseFloat(num);
+    if (value !== '' && value !== null && value !== undefined) {
+      money = parseFloat((+value * meneyMulty).toFixed(moneyDecimal));
+    }
     if (!isNaN(money)) return money;
     else return 0;
   };
 
-  divide = (
-    value: number | string,
-    multiple?: number,
-    decimal?: number,
-  ): { value: string; real: number; origin: number } => {
-    const moneyDecimal: number = decimal ?? this.dec;
-    const meneyMulty: number = multiple ?? this.multy;
-    const zero: string = Array(moneyDecimal).fill(0).join('');
-    let money: string = String(value);
-    let plain: number = 0;
+  divide = (value: number | string, multiple?: number, decimal?: number) => {
+    const moneyDecimal = decimal ?? this.dec;
+    const meneyMulty = multiple ?? this.multy;
+    const zero = Array(moneyDecimal).fill(0).join('');
+    let money = String(value);
+    let plain = 0;
 
     if (value !== '' && value !== null && value !== undefined) {
-      const split: string[] = String(+value / meneyMulty).split('.');
-      const num: number = +[split[0], '.', split[1] ? split[1].slice(0, moneyDecimal) : ''].join();
-      plain = parseFloat(num.toFixed(moneyDecimal));
+      const split = String(+value / meneyMulty).split('.');
+      const num = [...split[0], '.', split[1] ? split[1].slice(0, moneyDecimal) : ''].join('');
+      plain = parseFloat(num);
       const mount: string[] = String(plain).split('.');
 
       if (mount[1] !== undefined) {
@@ -46,7 +42,7 @@ class Money {
         mount[1] = zero;
       }
       mount[0] = mount[0].replace(/\d+/, function (n: string) {
-        return n.replace(/(\d)(?=(\d{3})+$)/g, function (value: string): string {
+        return n.replace(/(\d)(?=(\d{3})+$)/g, function (value) {
           return value + ',';
         });
       });
